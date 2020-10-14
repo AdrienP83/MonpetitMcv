e2c7592362e5dbe8a46a8478de54007b8278ed89
Adrien Poignard <poignard.adrien@gmail.com>
2020-10-14 17:19:57 +0200

    Création de la classe GestionClientModel

---
+++ src/Model/GestionClientModel.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GestionClientModel
+ *
+ * @author adrien.poignard
+ */
+namespace APP\Modele;
+class GestionClientModel {
+    //put your code here
+}
