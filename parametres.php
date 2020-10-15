41fe2ee1edc5e874e6c3ec4f7469b33b00cddd8c
Adrien Poignard <poignard.adrien@gmail.com>
2020-10-15 10:52:05 +0200

    ajout des parametres de la BD dans la classe parametres

--- parametres.php
+++ parametres.php
@@ -5,4 +5,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+define('DATABASE_URL',"localhost");
+define ('DATABASE_USER',"root");
+define ('DATABASE_PWD',"");
+define ('DATABSE_NAME',"clicom");
+define('CNSTRING',"mysql:host=".DATABASE_URL . ";dbname=" .DATABASE_NAME .";charset=UTF8" );
 
